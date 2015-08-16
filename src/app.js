bd0958bbb629f3a7e541888f466deb00dd3cb48d
Akash Badshah <akashbad@mit.edu>
2015-08-16 12:16:54 -0700

    forecast part 1

--- src/app.js
+++ src/app.js
@@ -1,43 +1,22 @@
 var UI = require('ui');
 var ajax = require('ajax');
+var Vector2 = require('vector2');
 
+// Show splash screen while waiting for data
+var splashWindow = new UI.Window();
 
-var card = new UI.Card({
-  title: 'Weather',
-  substitle: 'Fetching...'
+// Text element to inform user
+var text = new UI.Text({
+  position: new Vector2(0, 0),
+  size: new Vector2(144, 168),
+  text:'Downloading weather data...',
+  font:'GOTHIC_28_BOLD',
+  color:'black',
+  textOverflow:'wrap',
+  textAlign:'center',
+  backgroundColor:'white'
 });
 
-card.show();
-
-// Construct URL
-var cityName = 'Seattle';
-var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
-
-// Make the request
-ajax(
-  {
-    url: URL,
-    type: 'json'
-  },
-  function(data) {
-    // Success!
-    console.log('Successfully fetched weather data!');
-  
-    // Extract data
-    var location = data.name;
-    var temperature = Math.round(data.main.temp - 273.15) + 'C';
-  
-    // Always upper-case first letter of description
-    var description = data.weather[0].description;
-    description = description.charAt(0).toUpperCase() + description.substring(1);
-    
-    // Show to user
-    card.subtitle(location + ', ' + temperature);
-    card.body(description);
-  
-  },
-  function(error) {
-    // Failure!
-    console.log('Failed fetching weather data: ' + error);
-  }
-);
+// Add to splashWindow and show
+splashWindow.add(text);
+splashWindow.show();
