7d5912145a5ae4b154cfbdb6685bca099bace8f2
Akash Badshah <akashbad@mit.edu>
2015-08-16 12:02:16 -0700

    fetch weather

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 var UI = require('ui');
+var ajax = require('ajax');
+
 
 var card = new UI.card({
   title: 'Weather',
@@ -6,3 +8,36 @@ var card = new UI.card({
 });
 
 card.show();
+
+// Construct URL
+var cityName = 'Seattle';
+var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+
+// Make the request
+ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(data) {
+    // Success!
+    console.log('Successfully fetched weather data!');
+  
+    // Extract data
+    var location = data.name;
+    var temperature = Math.round(data.main.temp - 273.15) + 'C';
+  
+    // Always upper-case first letter of description
+    var description = data.weather[0].description;
+    description = description.charAt(0).toUpperCase() + description.substring(1);
+    
+    // Show to user
+    card.subtitle(location + ', ' + temperature);
+    card.body(description);
+  
+  },
+  function(error) {
+    // Failure!
+    console.log('Failed fetching weather data: ' + error);
+  }
+);
